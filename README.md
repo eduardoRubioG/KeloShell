0a8462ee17ceec755f0f9b8d2feaef8839e017c8
Eduardo <eduardo.rubio.jr85@gmail.com>
2026-06-28 14:02:04 -0400

    boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # KeloShell
+
+KeloShell is a blank PWA shell for new development.
+
+## Development
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/app-index.ts
@@ -0,0 +1,11 @@
+import { LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import './styles/global.css';
+
+@customElement('app-index')
+export class AppIndex extends LitElement {
+  render() {
+    return null;
+  }
+}
